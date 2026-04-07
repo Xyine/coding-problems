43eb94d9a2e1347bba714b30b20e18664a93e6ce
Cyrius <desagazancyrius@gmail.com>
2026-04-07 15:24:54 +0200

    feat: add c solution for 0001 Two Sum

---
+++ leetcode/easy/0001_two_sum/solution.c
@@ -0,0 +1,26 @@
+# include<stdlib.h>
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    *returnSize = 2;
+    int *soluce = malloc(2*sizeof(int));
+    
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i+1; j < numsSize; j++) {
+            if (i == j) {
+                continue;
+            }
+            else if (nums[i] + nums[j] == target) {
+                soluce[0] = i;
+                soluce[1] = j;
+                return soluce;
+            }
+        }
+    }
+    
+    *returnSize = 0;
+    return NULL;
+}
