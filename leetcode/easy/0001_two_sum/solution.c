9c70ed103b6a1b977ee5921f0bbbc48d2bb50caa
Cyrius <desagazancyrius@gmail.com>
2026-04-22 14:13:38 +0200

    fix: typo

--- leetcode/easy/0001_two_sum/solution.c
+++ leetcode/easy/0001_two_sum/solution.c
@@ -7,7 +7,7 @@
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     *returnSize = 2;
     int *soluce = malloc(2*sizeof(int));
-    
+
     for (int i = 0; i < numsSize; i++) {
         for (int j = i+1; j < numsSize; j++) {
             if (i == j) {
@@ -20,7 +20,7 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
             }
         }
     }
-    
+
     *returnSize = 0;
     return NULL;
 }
