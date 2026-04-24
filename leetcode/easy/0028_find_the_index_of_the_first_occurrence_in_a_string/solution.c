5b3d19959da5b694d741250f396e76b294207750
Cyrius <desagazancyrius@gmail.com>
2026-04-24 14:52:55 +0200

    feat: add solution for 0028 Find the Index of the First Occurrence in a String

---
+++ leetcode/easy/0028_find_the_index_of_the_first_occurrence_in_a_string/solution.c
@@ -0,0 +1,34 @@
+#include <string.h>
+
+int strStr(char *haystack, char *needle) {
+    char *pos = strstr(haystack, needle);
+
+    if (pos == NULL) {
+        return -1;
+    }
+
+    return (int)(pos - haystack);
+}
+
+int strStr(char *haystack, char *needle) {
+    if (needle[0] == '\0') {
+        return 0;
+    }
+
+    int n = strlen(haystack);
+    int m = strlen(needle);
+
+    for (int i = 0; i <= n - m; i++) {
+        int j = 0;
+
+        while (j < m && haystack[i + j] == needle[j]) {
+            j++;
+        }
+
+        if (j == m) {
+            return i;
+        }
+    }
+
+    return -1;
+}
