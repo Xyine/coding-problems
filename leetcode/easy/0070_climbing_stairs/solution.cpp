6277c440341147abf7cfd3112039804eaed5360b
Cyrius <desagazancyrius@gmail.com>
2026-04-25 14:31:07 +0200

    feat: add solution for 0070 climbing stairs

---
+++ leetcode/easy/0070_climbing_stairs/solution.cpp
@@ -0,0 +1,12 @@
+int climbStairs(int n) {
+    int a = 1;
+    int b = 1;
+
+    for (int i = 0; i < n; i++) {
+        int temp = a;
+        a = b;
+        b = temp + b;
+    }
+
+    return a;
+}
---
+++ leetcode/easy/0070_climbing_stairs/solution.py
@@ -0,0 +1,7 @@
+def climbStairs(n: int) -> int:
+    a, b = 1, 1
+
+    for _ in range(n):
+        a, b = b, a + b
+
+    return a
