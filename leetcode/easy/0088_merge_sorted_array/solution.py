3281a12b8cfa4598c97ae32b7251d7e2531f8bc8
Cyrius <desagazancyrius@gmail.com>
2026-04-26 18:19:26 +0200

    feat: add solution for 0088 Merge Sorted Array

---
+++ leetcode/easy/0088_merge_sorted_array/solution.py
@@ -0,0 +1,5 @@
+def merge(nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+    """
+    Do not return anything, modify nums1 in-place instead.
+    """
+    nums1[:] = sorted(nums1[:m] + nums2)
