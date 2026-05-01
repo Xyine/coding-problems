c0a5f07a106aa1315bc7b7993f08ab0d364051e0
Cyrius <desagazancyrius@gmail.com>
2026-05-01 16:50:34 +0200

    fix: temp variable

--- leetcode/easy/0119_Pascal_triangle_II/solution.c
+++ leetcode/easy/0119_Pascal_triangle_II/solution.c
@@ -5,11 +5,17 @@ int* getRow(int rowIndex, int* returnSize) {
     *returnSize = rowIndex + 1;
     int* row = malloc(*returnSize * sizeof(int));
 
+    long temp=1;
     row[0] = 1;
 
     for (int i = 1; i < *returnSize; i++) {
-        row[i] = row[i -1] * (*returnSize - i) / i;
+        temp= temp * (*returnSize - i) / i;
+        row[i] =  temp;
     }
 
     return row;
 }
+
+// Note: We didnt actually need the variable temp. 
+// But the test cases are such that multiplying in one case exceeds the int range, 
+// and since we cannot change return type we have to take the long data type variable as temporary.
