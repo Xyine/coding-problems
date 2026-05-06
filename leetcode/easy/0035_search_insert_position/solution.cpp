ffa5c951ec518e1a6fc108b0f8ba8c092a11c266
Cyrius <desagazancyrius@gmail.com>
2026-05-06 10:30:08 +0200

    feat: add a solution for 0035 Search Insert Position in C++

--- leetcode/easy/0035_search_insert_position/solution.cpp
+++ leetcode/easy/0035_search_insert_position/solution.cpp
@@ -5,3 +5,19 @@ using namespace std;
 int searchInsert(vector<int>& nums, int target) {
     return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
 }
+
+int searchInsert(vector<int>& nums, int target) {
+    int left = 0, right = nums.size();
+
+    while (left < right) {
+        int mid = (left + right) / 2;
+
+        if (target > nums[mid]) {
+            left = mid + 1;
+        }
+        else {
+            right = mid;
+        }
+    }
+    return left;
+}
