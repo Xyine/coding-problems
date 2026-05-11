45f46f5202152adf0862ac979e921eb612f9c042
Cyrius <desagazancyrius@gmail.com>
2026-05-11 13:08:01 +0200

    feat: add solution for 0145 Binary Tree Postorder Traversal in C++

---
+++ leetcode/easy/0145_binary_tree_postorder_traversal/solution.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void postorder(TreeNode *root, std::vector<int>& result) {
+        if (root == nullptr) return;
+        postorder(root->left, result);
+        postorder(root->right, result);
+        result.push_back(root->val);
+    }
+
+    std::vector<int> postorderTraversal(TreeNode* root) {
+        std::vector<int> result;
+        postorder(root, result);
+        return result;
+    }
+};
