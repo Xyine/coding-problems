144a79d519ce0472b7f7b464937a47e57141acc8
Cyrius <desagazancyrius@gmail.com>
2026-05-18 11:14:31 +0200

    feat: add solution for 0183 Customers Who Never Order in SQL

---
+++ leetcode/easy/0183_customers_who_never_order/solution.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below
+SELECT Customers.name AS Customers
+FROM Customers
+LEFT JOIN Orders ON Customers.id = Orders.customerId
+WHERE Orders.customerId IS NULL;
+
+SELECT name AS Customers
+FROM Customers
+WHERE id NOT IN (SELECT customerId FROM Orders);
