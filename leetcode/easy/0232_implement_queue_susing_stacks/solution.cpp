24ca28dd9ea441e0b381036728bf0bbd46a22bfc
Cyrius <desagazancyrius@gmail.com>
2026-06-05 11:44:04 +0200

    feat: add solution for 232 Implement Queue using Stacks in C++

---
+++ leetcode/easy/0232_implement_queue_susing_stacks/solution.cpp
@@ -0,0 +1,37 @@
+#include <stack>
+using namespace std;
+
+class MyQueue {
+private:
+    stack<int> input;
+    stack<int> output;
+
+public:
+    MyQueue() {
+    }
+
+    void push(int x) {
+        input.push(x);
+    }
+
+    int pop() {
+        int value = peek();
+        output.pop();
+        return value;
+    }
+
+    int peek() {
+        if (output.empty()) {
+            while (!input.empty()) {
+                output.push(input.top());
+                input.pop();
+            }
+        }
+
+        return output.top();
+    }
+
+    bool empty() {
+        return input.empty() && output.empty();
+    }
+};
